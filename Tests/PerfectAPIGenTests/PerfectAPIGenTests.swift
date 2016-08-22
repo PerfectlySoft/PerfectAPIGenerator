9c5e373f780c9300374baf09fea86af3d61df924
Jonathan Guthrie <jono@perfect.org>
2016-08-22 15:25:33 -0400

    update file naming to remove conflicts

--- Tests/PerfectAPIGenTests/PerfectAPIGenTests.swift
+++ Tests/PerfectAPIGenTests/PerfectAPIGenTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import PerfectAPIGen
+@testable import PerfectAPIGenTests
 
 class PerfectAPIGenTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(PerfectAPIGen().text, "Hello, World!")
+        XCTAssertEqual(PerfectAPIGenTests().text, "Hello, World!")
     }
 
 
