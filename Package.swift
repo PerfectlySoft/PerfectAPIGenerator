821398e243161abcec7c761e752d1a89c3b170df
Jonathan Guthrie <jono@perfect.org>
2016-08-22 15:24:58 -0400

    rename lib

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
 	name: "PerfectAPIGen",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect.git", versions: Version(0,0,0)..<Version(10,0,0)),
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", versions: Version(0,0,0)..<Version(10,0,0)),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", versions: Version(0,0,0)..<Version(10,0,0))
 	],
 	exclude: []
