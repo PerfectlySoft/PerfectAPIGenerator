083cfab5e4958382ce6509eb15c0a79fa69f1d57
Jonathan Guthrie <jono@perfect.org>
2016-11-22 17:13:45 -0500

    most current api

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
 	],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0)
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1)
 	],
 	exclude: []
 )
--- Sources/perfectapigen/main.swift
+++ Sources/perfectapigen/main.swift
@@ -88,82 +88,56 @@ func runProc(cmd: String, args: [String], read: Bool = false) throws -> String? 
 	return ret
 }
 
-//	"Perfect-FastCGI/":"PerfectFastCGI",
-let repoList = [
-	"Perfect-CURL/":"PerfectCURL",
-	"Perfect-HTTP/":"PerfectHTTP",
-	"Perfect-HTTPServer/":"PerfectHTTPServer",
-	"Perfect-MongoDB/":"MongoDB",
-	"Perfect-Mustache/":"PerfectMustache",
-	"Perfect-MySQL/":"MySQL",
-	"Perfect-Net/":"PerfectNet",
-	"Perfect-Notifications/":"PerfectNotifications",
-	"Perfect-PostgreSQL/":"PostgreSQL",
-	"Perfect-Redis/":"PerfectRedis",
-	"Perfect-SQLite/":"SQLite",
-	"Perfect-Thread/":"PerfectThread",
-	"Perfect-WebSockets/":"PerfectWebSockets",
-	"PerfectLib/":"PerfectLib"
-]
+let repositoryBaseURL = "https://github.com/PerfectlySoft/"
 
-//	"Perfect-FastCGI/",
 let repoListOrdered = [
-	"PerfectLib/",
-	"Perfect-CURL/",
-	"Perfect-HTTP/",
-	"Perfect-HTTPServer/",
-	"Perfect-MongoDB/",
-	"Perfect-Mustache/",
-	"Perfect-MySQL/",
-	"Perfect-Net/",
-	"Perfect-Notifications/",
-	"Perfect-PostgreSQL/",
-	"Perfect-Redis/",
-	"Perfect-SQLite/",
-	"Perfect-Thread/",
-	"Perfect-WebSockets/"
+
+	("PerfectLib/","PerfectLib"),
+	("Perfect-CURL/","PerfectCURL"),
+	("Perfect-Filemaker/","FileMaker"),
+	("Perfect-HTTP/","PerfectHTTP"),
+	("Perfect-HTTPServer/","PerfectHTTPServer"),
+	("Perfect-Logger/","Logger"),
+	("Perfect-MongoDB/","MongoDB"),
+	("Perfect-Mustache/","PerfectMustache"),
+	("Perfect-MySQL/","MySQL"),
+	("Perfect-MariaDB/","MariaDB"),
+	("Perfect-Net/","PerfectNet"),
+	("Perfect-Notifications/","PerfectNotifications"),
+	("Perfect-PostgreSQL/","PostgreSQL"),
+	("Perfect-Redis/","PerfectRedis"),
+	("Perfect-RequestLogger/","RequestLogger"),
+	("Perfect-SQLite/","SQLite"),
+	("Perfect-Thread/","PerfectThread"),
+	("Perfect-Turnstile-PostgreSQL/","Perfect-Turnstile-PostgreSQL"),
+	("Perfect-Turnstile-SQLite/","Perfect-Turnstile-SQLite"),
+	("Perfect-WebSockets/","PerfectWebSockets"),
+	("Perfect-XML/","XML"),
+	("Perfect-Zip/","Zip")
+
 ]
 
-
-/*
-
-let repoList = [
-"PerfectLib/":"PerfectLib",
-"Perfect-CURL/":"PerfectCURL"
-
-]
-
-let repoList = [
-	"Perfect-CURL/":"PerfectCURL",
-	"Perfect-FastCGI/":"PerfectFastCGI",
-	"Perfect-HTTP/":"PerfectHTTP",
-	"Perfect-HTTPServer/":"PerfectHTTPServer",
-	"Perfect-MongoDB/":"MongoDB",
-	"Perfect-Mustache/":"PerfectMustache",
-	"Perfect-MySQL/":"MySQL",
-	"Perfect-Net/":"PerfectNet",
-	"Perfect-Notifications/":"PerfectNotifications",
-	"Perfect-PostgreSQL/":"PostgreSQL",
-	"Perfect-Redis/":"PerfectRedis",
-	"Perfect-SQLite/":"SQLite",
-	"Perfect-Thread/":"PerfectThread",
-	"Perfect-WebSockets/":"PerfectWebSockets",
-	"PerfectLib/":"PerfectLib"
-]
-
-*/
 
 let workingDir = Dir.workingDir
 
+// find a better place for this
+var undocumentedElements = [[String:Any]]()
+
+
 func processAPISubstructure(_ substructures: [Any]) -> [[String:Any]]? {
 
+	if substructures.count == 0 {
+		return nil
+	}
+//	print(substructures)
+
 	func cleanKey(_ key: String) -> String {
-		if key.hasPrefix("key.") {
-			return key[key.index(key.startIndex, offsetBy: 4)..<key.endIndex]
-		}
+		//		if key.hasPrefix("key.") {
+		//			return key[key.index(key.startIndex, offsetBy: 4)..<key.endIndex]
+		//		}
 		return key
 	}
-	
+
 	func getType(_ value: String) -> String {
 		switch value {
 		case "source.lang.swift.decl.var.instance":
@@ -185,11 +159,13 @@ func processAPISubstructure(_ substructures: [Any]) -> [[String:Any]]? {
 		case "source.lang.swift.decl.extension":
 			return "extension"
 		case "source.lang.swift.decl.function.method.instance":
-			return "method"
+			return "func"
 		case "source.lang.swift.decl.function.method.static":
-			return "static method"
+			return "static func"
+		case "source.lang.swift.decl.function.subscript":
+			return "subscript"
 		case "source.lang.swift.decl.function.free":
-			return "function"
+			return "func"
 		case "source.lang.swift.decl.enum":
 			return "enum"
 		case "source.lang.swift.decl.enumcase": // filtered out
@@ -200,7 +176,7 @@ func processAPISubstructure(_ substructures: [Any]) -> [[String:Any]]? {
 			fatalError("Unknown type \(value)")
 		}
 	}
-	
+
 	var retAry = [[String:Any]]()
 	top:
 	for substructure in substructures {
@@ -212,51 +188,116 @@ func processAPISubstructure(_ substructures: [Any]) -> [[String:Any]]? {
 		var wasEnumElement = false
 		var wasEnumCase = false
 		var subSubstructure: [Any]?
-		
+		var wasDocumented = false
+		var simpleKind = ""
 		for (key, value) in substructure {
 			let key = cleanKey(key)
 			switch key {
-			case "accessibility":
-				if let value = value as? String {
-					guard value == "source.lang.swift.accessibility.public" || value == "source.lang.swift.accessibility.internal" else {
+			case "key.accessibility":
+				guard let value = value as? String,
+					value == "source.lang.swift.accessibility.public" || value == "source.lang.swift.accessibility.internal" else {
+						continue top
+				}
+				wasInternal = value == "source.lang.swift.accessibility.internal"
+			case "key.substructure":
+				subSubstructure = value as? [Any]
+			case "key.attributes":
+				guard let attrAry = value as? [[String:Any]] else {
+					continue
+				}
+				for attr in attrAry {
+					if let keyAttribute = attr["key.attribute"] as? String, keyAttribute == "source.decl.attribute.available" {
 						continue top
 					}
-					wasInternal = value == "source.lang.swift.accessibility.internal"
 				}
-			case "doc.name", "name", "doc.comment",
-			     "parsed_declaration", "typename":
-				subDict[key] = value
-			case "kind":
+
+			case "key.doc.declaration":
+				subDict[key] = (value as? String)?.replacingOccurrences(of: ": <<error type>>", with: "").replacingOccurrences(of: "-> <<error type>>", with: "") ?? ""
+			case "key.doc.full_as_xml", "key.fully_annotated_decl", "key.annotated_decl":
+				continue
+			case "key.doc.parameters", "key.doc.discussion", "key.doc.result_discussion":
+				// we don't handle these but it might be a good idea to do so
+				continue
+			case "key.kind":
 				if let value = value as? String {
-					subDict[key] = getType(value)
+					simpleKind = getType(value)
+					subDict["key.kind.simple"] = simpleKind
 					wasEnumElement = value == "source.lang.swift.decl.enumelement"
 					wasEnumCase = value == "source.lang.swift.decl.enumcase"
+					subDict["key.isextension"] = (simpleKind == "extension")
 				}
-			case "substructure":
-				subSubstructure = value as? [Any]
+				fallthrough
 			default:
-				()
+				subDict[key] = value
 			}
 		}
+
+
+		if let parsed_declaration = subDict["key.parsed_declaration"] {
+			var decl = parsed_declaration as! String
+			if decl.hasPrefix("public ") {
+				decl = decl.stringByReplacing(string: "public ", withString: "")
+			}
+			if decl.hasPrefix("final ") {
+				decl = decl.stringByReplacing(string: "final ", withString: "")
+			}
+			if let kind = subDict["kind"] {
+				if decl.hasPrefix("\(kind) ") {
+					decl = decl.stringByReplacing(string: "\(kind) ", withString: "")
+				}
+			}
+
+			let prefixes = ["func ","let ","static let ","case ","static var ","var ","class ","static ", "struct ", "enum ", "protocol ", "static func "]
+			for pf in prefixes {
+				if decl.hasPrefix(pf) {
+					decl = decl.stringByReplacing(string: pf, withString: "")
+				}
+			}
+
+			if decl.hasSuffix("{}") {
+				decl = decl.stringByReplacing(string: "{}", withString: "")
+			}
+
+			subDict["key.parsed_declaration"] = decl
+			subDict["key.name"] = decl
+
+		}
+
 		guard !wasInternal || wasEnumElement else {
 			continue
 		}
 		guard subDict.count > 0 else {
 			continue
 		}
+		let docDeclTest = subDict["key.doc.declaration"] as? String
+		if nil == docDeclTest || docDeclTest!.isEmpty {
+			subDict["key.doc.declaration"] = subDict["key.parsed_declaration"]
+		}
+		wasDocumented = (nil != subDict["key.doc.comment"]) || simpleKind == "extension" // extension blocks arguably do not need docs
+		if !wasDocumented {
+			subDict["key.doc.comment"] = ""
+		}
+		if simpleKind == "case", let name = subDict["key.name"] {
+			subDict["key.doc.declaration"] = "case \(name)"
+		}
+
 		if let subSubstructure = subSubstructure, let ss = processAPISubstructure(subSubstructure) {
 			if wasEnumCase {
 				retAry.append(contentsOf: ss)
 				continue
 			} else {
-				subDict["substructure"] = ss
+				subDict["key.substructure"] = ss
 			}
-		} else if let kind = subDict["kind"] as? String , kind == "extension" {
+		} else if simpleKind == "extension" {
 			continue
 		} else {
-			subDict["substructure"] = [[String:Any]]()
+			subDict["key.substructure"] = [[String:Any]]()
 		}
 		retAry.append(subDict)
+
+		if !wasDocumented {
+			undocumentedElements.append(subDict)
+		}
 	}
 	guard retAry.count > 0 else {
 		return nil
@@ -279,8 +320,20 @@ func processAPIInfo(projectName: String, apiInfo: [Any]) -> [[String:Any]] {
 			}
 			if let sub = processAPISubstructure(substructure) {
 				var projDict = [String:Any]()
-				projDict["file"] = file
-				projDict["substructure"] = sub
+				var repoFileComponents = [String]()
+				var foundSources = false
+				for component in file.filePathComponents {
+					if component == "Sources" {
+						foundSources = true
+					}
+					if foundSources {
+						repoFileComponents.append(component)
+					}
+				}
+				let joinedFile = repoFileComponents.joined(separator: "/")
+				projDict["key.repo.file"] = joinedFile
+				projDict["key.repo.path"] = "blob/master/" + joinedFile
+				projDict["key.substructure"] = sub
 				projectsList.append(projDict)
 			}
 		}
@@ -313,44 +366,47 @@ func getAllSwiftFiles(inDir: Dir) throws -> [String] {
 	return ret
 }
 
-for name in repoListOrdered {
-	guard let target = repoList[name] else {
-		break
-	}
+for (name, target) in repoListOrdered {
 
 	print("Working in: \(name)")
 
 	let repoDirPath = srcs + "/" + name
 	let repoDir = Dir(repoDirPath)
 	try repoDir.setAsWorkingDir()
-	
+
 	let sourcekitten = "/usr/local/bin/sourcekitten"
 	let skArgs1 = ["doc", "--single-file"]
-	let skArgs2 = ["--", "-c"]
-	
+	let skArgs2 = ["--", "-c"] // YOU MUST ADD THESE FLAGS or sourcekit will not give you doc comment info for the single-file
+
 	let git = "git"
 	let gitPullArgs = ["pull"]
-	
+
 	do {
 		_ = try runProc(cmd: git, args: gitPullArgs)
 		let files = try getAllSwiftFiles(inDir: Dir(repoDirPath + "Sources"))
 		var decodedApiInfo = [Any]()
 		for swiftFile in files {
 			let apiInfo = try runProc(cmd: sourcekitten, args: skArgs1 + [swiftFile] + skArgs2 + [swiftFile], read: true)
+			//			print("\(apiInfo)")
 			let thisDecodedApiInfo = try apiInfo?.jsonDecode() as? [String:Any]
 			decodedApiInfo.append(thisDecodedApiInfo as Any)
 		}
 		let projectAPI = processAPIInfo(projectName: name, apiInfo: decodedApiInfo)
 		var projectInfo = [String:Any]()
-		projectInfo["name"] = fixProjectName(name)
-		projectInfo["files"] = projectAPI
+
+		let repoName = repositoryBaseURL + name
+		projectInfo["key.repo"] = repoName
+		projectInfo["key.name"] = fixProjectName(name)
+		projectInfo["key.files"] = projectAPI
 		projectsAry.append(projectInfo)
 	} catch {
 		print("GOT EXCEPTION \(error)")
 	}
 }
 
-let resDict: [String:Any] = ["projects":projectsAry]
+
+let resDict: [String:Any] = ["key.projects":projectsAry, "key.undocumented":undocumentedElements]
+//let resDict: [String:Any] = ["projects":projectsAry]
 
 try workingDir.setAsWorkingDir()
 
