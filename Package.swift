2b6cad7d800ae4cf26377e1788332acef3c8aa89
Rockford Wei <rocky@perfect.org>
2018-01-30 12:56:28 -0500

    Fixing obsolete url.

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
 		Target(name: "perfectapigen", dependencies: [])
 	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
 		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2)
 	],
 	exclude: []
